29c990498bbd09e88d78a216a450556d035b7cf2
Zbyszek Kisły <zbyshekh@gmail.com>
2015-02-10 03:08:40 +0100

    index, config

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/** The engine used by the database */
+define('DB_ENGINE', 'mysql');
+
+/** The name of the database */
+define('DB_NAME', 'owoce');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** salt, for hashing passwords, 
+feel free to change, but ONLY BEFORE creating any users */
+define('DB_SALT', 'ldbasjdkjsd');
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+if(!@include 'config.php'){
+    echo "zrobic generator config.php!";
+}else{
+    $db=new PDO(DB_ENGINE.':dbname='.DB_NAME.";host=".DB_HOST, DB_USER, DB_PASSWORD);
+    require '../libs/Smarty.class.php';
+    function __autoload($class_name)
+    {
+        # List all the class directories in the array.
+        $folders = array(
+            'models/',
+            'views/',
+            'controllers/',
+            ''
+        );
+
+        foreach($folders as $path){
+            $file = sprintf('%s%s.php',  $path, $class_name);
+            if(is_file($file)){
+                include_once $file;
+            }
+            //echo $file.'<br>';
+        }
+    }
+
+    spl_autoload_register('__autoload');
+
+
+}
+
